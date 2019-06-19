0684a5dbb7376e9931f5c2899887fee178fd7068
Rybre <qjaqns1143@naver.com>
2019-06-20 03:30:31 +0900

    초기페이지 설정완료 아래 로그는 초기페이지 설정 중
    몽고 db설정 데이터 업데이트임

--- mongoDB/src/main/java/com/park/springboot/HeloController.java
+++ mongoDB/src/main/java/com/park/springboot/HeloController.java
@@ -36,12 +36,73 @@ public class HeloController {
 
 		mav.setViewName("index");
 		mav.addObject("title", "학점 평균 계산기");
-		mav.addObject("msg", "학점평균을 입력해주세요.");
+		mav.addObject("msg", "학점 평균을 입력해주세요.");
 
 		Iterable<MyDataMongo> list = repository.findAll();
 		mav.addObject("datalist", list);
 		return mav;
 	}
+	
+	/**
+	 *
+	 * @fn 		public ModelAndView insert(ModelAndView mav)
+	 * 
+	 * @brief 	입력 페이지 설정 
+	 *
+	 * @author 	박범찬
+	 * @date 	2019-06-20
+	 *
+	 * @param	mav ModelAndView
+	 *
+	 * @remark 	웹 페이지를 불러오기 위한 초기페이지 설정
+	 *
+	 */
+	
+	@RequestMapping(value = "/insert", method = RequestMethod.GET)
+	public ModelAndView insert(ModelAndView mav) {
+		mav.setViewName("insert");
+		mav.addObject("title", "성적 입력");
+		mav.addObject("msg", "성적을 입력해주세요");
 
+		return mav;
+	}
+		
+	/**
+	 *
+	 * @fn 		public ModelAndView insert(ModelAndView mav)
+	 * 
+	 * @brief 	입력 페이지 설정 
+	 *
+	 * @author 	박범찬
+	 * @date 	2019-06-20
+	 *
+	 * @param 	mav ModelAndView
+	 *
+	 * @remark	폼에 입력된 값을 받아온 후 객체에 저장	
+	 *		   	save 메소드를 이용해 DB에 저장			
+	 *
+	 */
 	
-	
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public ModelAndView form(
+			@RequestParam("code") String code, 
+			@RequestParam("name") String name,
+			@RequestParam("UI") int UI, 
+			@RequestParam("Culture") int Culture,
+			@RequestParam("Capstone") int Capstone, 
+			@RequestParam("Jquery") int Jquery, 
+			@RequestParam("Pass") int Pass,
+			@RequestParam("Mento") int Mento, 
+			@RequestParam("Smart1") int Smart1, 
+			@RequestParam("Smart2") int Smart2,
+			@RequestParam("Jsp") int Jsp, 
+			@RequestParam("Linux") int Linux, 
+
+			ModelAndView mov) 
+	{
+		MyDataMongo mydata = new MyDataMongo(name, code, UI, Capstone, Jquery, Pass, Mento, Smart1, Smart2, Jsp, Linux);
+		repository.save(mydata);
+		
+		return new ModelAndView("redirect:/");
+	}
+
