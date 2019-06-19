6dcb458c68396f2ba31ef8a18ab82f1f83eb2f7a
Rybre <qjaqns1143@naver.com>
2019-06-20 03:51:58 +0900

    데이터 리포지터리 추가

---
+++ mongoDB/src/main/java/com/park/springboot/repositories/MyDataMongoRepository.java
@@ -0,0 +1,13 @@
+package com.park.springboot.repositories;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.park.springboot.MyDataMongo;
+
+public interface MyDataMongoRepository extends MongoRepository<MyDataMongo, Long> 
+{
+	public List<MyDataMongo> findById(String k);
+	public void deleteById(String param);
+}
